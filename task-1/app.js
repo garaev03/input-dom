373f8d32333c368cae0ad3d2cfb9de447cd1bd36
garaevo3 <5mptrbf@code.edu.az>
2022-11-14 14:04:06 +0400

    updated photos card, added animation

--- task-1/app.js
+++ task-1/app.js
@@ -1,6 +1,6 @@
 const BTN=document.getElementById("BTN")
 const body=document.querySelector("body")
-// const mode=document.getElementById("mode")
+const mode=document.getElementById("mode")
 const input=document.getElementById("input")
 const box=document.getElementById("box")  
 const buttons=document.getElementById("buttons")   
@@ -10,17 +10,18 @@ let count=0
 
 BTN.addEventListener("click", btn)
 BTN.addEventListener("click", clear)
-// mode.addEventListener("click",changeMode)
+mode.addEventListener("click",changeMode)
 
-
-// function changeMode(){
-//     body.style.backgroundColor="black"
-//     body.style.color="white"
-// }
+function changeMode(){
+    mode.innerHTML="<i style='color: white;border-color: black;border-radius: 0.25em;'  class='fa-solid fa-moon'></i> "
+    var element=document.body
+    element.tagName.toggle(body)
+}
+// mode.innerHTML="<i style='border-color: black;border-radius: 0.25em'class='fa-solid fa-moon ></i>"
 function clear(){
     if(count==0){
     const  button=document.createElement("button")
-    button.setAttribute("class","btn btn-outline-danger btn-sm")
+    button.setAttribute("class","rounded-0 btn btn-outline-danger btn-sm")
     button.style.width="82px"
     button.innerHTML="Clear!"
     buttons.appendChild(button) 
@@ -32,7 +33,7 @@ function clear(){
 function btn() {
     let p=document.createElement("p")
     p.innerHTML+=input.value
-    p.setAttribute("class","ms-5 ps-3")
+    p.setAttribute("class","ms-5 ps-4")
     box.appendChild(p)
     p.addEventListener("click",pclicked)
     input.value=""
--- task-2/app.js
+++ task-2/app.js
@@ -5,86 +5,67 @@ const album=document.getElementById("album")
 const todo=document.getElementById("todo")
 const user=document.getElementById("user")
 const div=document.getElementById("emptField")
-let string, i
 
+var string, i, response
 
 post.addEventListener("click",getPost)
-comment.addEventListener("click",getComment)
+comment.addEventListener("click",getComment) 
 photo.addEventListener("click",getPhoto)
 album.addEventListener("click",getAlbum)
 todo.addEventListener("click",getTodo)
 user.addEventListener("click",getUser)
 
+function clearBTN(){
+let clearbtn=document.createElement("button")
+clearbtn.innerHTML="Clear"
+clearbtn.setAttribute("class","rounded-0 btn btn-outline-danger")
+div.appendChild(clearbtn)
+clearbtn.addEventListener("click",()=>{
+    div.innerHTML=""
+})
+}
 
 function getPost(){
     div.innerHTML=""
 
-    let clearbtn=document.createElement("button")
-    clearbtn.innerHTML="Clear"
-    clearbtn.setAttribute("class","rounded-0 btn btn-outline-danger ")
-    div.appendChild(clearbtn)
-    clearbtn.addEventListener("click",()=>{
-        div.innerHTML=""
-    })
-
+    clearBTN()
 
     let ol=document.createElement("ol")
     div.appendChild(ol)
     ol.setAttribute("class","m-2")
     axios.get("https://jsonplaceholder.typicode.com/posts")
     .then(resp =>{
-        for(i=1;i<=resp.data.length;i++){
-            axios.get(`https://jsonplaceholder.typicode.com/posts/${i}`)
-                .then(response => {
-
+        for(i=1;i<=resp.data.length-1;i++){
+                    response=resp.data[i]
                     let li=document.createElement("li")
                     ol.appendChild(li)
-
-                    li.innerHTML="userId: "+response.data.userId+"<br>"+"id: "+response.data.id+"<br>"+"title: "+response.data.title+"<br>"+"body: "+response.data.body+"<hr>"
-                    
+                    li.innerHTML="userId: "+response.userId+"<br>"+"id: "+response.id+"<br>"+"title: "+response.title+"<br>"+"body: "+response.body+"<hr>"  
                     }   
-                    )}              
+                                  
     })  
 }
 function getComment(){
     div.innerHTML=""
 
-    let clearbtn=document.createElement("button")
-    clearbtn.innerHTML="Clear"
-    clearbtn.setAttribute("class","rounded-0 btn btn-outline-danger")
-    div.appendChild(clearbtn)
-    clearbtn.addEventListener("click",()=>{
-        div.innerHTML=""
-    })
+    clearBTN()
 
     let ol=document.createElement("ol")
     div.appendChild(ol)
     ol.setAttribute("class","m-2")
     axios.get("https://jsonplaceholder.typicode.com/comments")
     .then(resp =>{
-        for(i=1;i<=resp.data.length;i++){
-            axios.get(`https://jsonplaceholder.typicode.com/comments/${i}`)
-                .then(response => {
-
+        for(i=1;i<=resp.data.length-1;i++){
+                    response=resp.data[i]
                     let li=document.createElement("li")
                     ol.appendChild(li)
-
-                    li.innerHTML="postId: "+response.data.postId+"<br>"+"id: "+response.data.id+"<br>"+"name: "+response.data.name+"<br>"+"email: "+response.data.email+"<br>"+"body: "+response.data.body+"<hr>"
-                    
+                    li.innerHTML="postId: "+response.postId+"<br>"+"id: "+response.id+"<br>"+"name: "+response.name+"<br>"+"email: "+response.email+"<br>"+"body: "+response.body+"<hr>"                    
                     }   
-                    )}              
-    })  
+                    })                   
 }
 function getPhoto(){
     div.innerHTML=""
 
-    let clearbtn=document.createElement("button")
-    clearbtn.innerHTML="Clear"
-    clearbtn.setAttribute("class","rounded-0 btn btn-outline-danger")
-    div.appendChild(clearbtn)
-    clearbtn.addEventListener("click",()=>{
-        div.innerHTML=""
-    })
+    clearBTN()
 
     let ol=document.createElement("ol")
     div.appendChild(ol)
@@ -93,11 +74,9 @@ function getPhoto(){
     axios.get("https://jsonplaceholder.typicode.com/photos")
     .then(resp =>{
         alert("ZƏHMƏT OLMASA ŞƏKİLLƏR YÜKLƏNƏNƏ KİMİ GÖZLƏYİN")
-        for(i=1;i<=resp.data.length;i++){
-            axios.get(`https://jsonplaceholder.typicode.com/photos/${i}`)
-                .then(response => {
-
-                    let dv=document.createElement("dv")
+        for(i=1;i<=resp.data.length-1;i++){
+                    response=resp.data[i]
+                    let dv=document.createElement("div")
                     let li=document.createElement("li")
                     let p2=document.createElement("p")
                     dv.setAttribute("class","d-flex  justify-content-between")
@@ -107,16 +86,14 @@ function getPhoto(){
                     dv.appendChild(li)
                     dv.appendChild(p2)
 
-                    li.innerHTML="albumId: "+response.data.albumId+"<br>"+"id: "+response.data.id+"<br>"+"title: "+response.data.title+"<br>"+"url: "+"<a target='blank' href="+response.data.url+">"+response.data.url+"<br>"
-                    p2.innerHTML="<img class='justify-self-end' src="+response.data.thumbnailUrl+"<hr>"
+                    li.innerHTML="albumId: "+response.albumId+"<br>"+"id: "+response.id+"<br>"+"title: "+response.title+"<br>"+"url: "+"<a target='blank' href="+response.url+">"+response.url+"<br>"
+                    p2.innerHTML="<img class='justify-self-end' src="+response.thumbnailUrl+"<hr>"
 
                     let hr=document.createElement("hr")
                     hr.setAttribute("class","m-1")
                     ol.appendChild(hr)
                    
-                    }   
-                    )}         
-                    
+                    }            
     })  
 }
 function getAlbum(){
@@ -135,83 +112,60 @@ function getAlbum(){
     ol.setAttribute("class","m-2")
     axios.get("https://jsonplaceholder.typicode.com/albums")
     .then(resp =>{
-        for(i=1;i<=resp.data.length;i++){
-            axios.get(`https://jsonplaceholder.typicode.com/albums/${i}`)
-                .then(response => {
+        for(i=1;i<=resp.data.length-1;i++){
+            response=resp.data[i]
+            let li=document.createElement("li")
+            ol.appendChild(li)
 
-                    let li=document.createElement("li")
-                    ol.appendChild(li)
-
-                    li.innerHTML="userID: "+response.data.userId+"<br>"+"id: "+response.data.id+"<br>"+"title: "+response.data.title+"<br>"+"<hr>"
+            li.innerHTML="userID: "+response.userId+"<br>"+"id: "+response.id+"<br>"+"title: "+response.title+"<br>"+"<hr>"
                     
-                    }   
-                    )}              
+        }                         
     })  
 }
 function getTodo(){
     div.innerHTML=""
 
-    let clearbtn=document.createElement("button")
-    clearbtn.innerHTML="Clear"
-    clearbtn.setAttribute("class","rounded-0 btn btn-outline-danger")
-    div.appendChild(clearbtn)
-    clearbtn.addEventListener("click",()=>{
-        div.innerHTML=""
-    })
+    clearBTN()
 
     let ol=document.createElement("ol")
     div.appendChild(ol)
     ol.setAttribute("class","m-2")
     axios.get("https://jsonplaceholder.typicode.com/todos")
     .then(resp =>{
-        for(i=1;i<=resp.data.length;i++){
-            axios.get(`https://jsonplaceholder.typicode.com/todos/${i}`)
-                .then(response => {
-                    
-                    let li=document.createElement("li")
-                    ol.appendChild(li)
-                    li.innerHTML="userID: "+response.data.userId+"<br>"+"id: "+response.data.id+"<br>"+"title: "+response.data.title+"<br>"+"completed: "+response.data.completed+"<hr>"
-                    }   
-                    )}              
+        for(i=1;i<=resp.data.length-1;i++){
+            response=resp.data[i]
+            let li=document.createElement("li")
+            ol.appendChild(li)
+            li.innerHTML="userID: "+response.userId+"<br>"+"id: "+response.id+"<br>"+"title: "+response.title+"<br>"+"completed: "+response.completed+"<hr>"
+            }                
     })  
 }
 function getUser(){
     div.innerHTML=""
-
-    let clearbtn=document.createElement("button")
-    clearbtn.innerHTML="Clear"
-    clearbtn.setAttribute("class","rounded-0 btn btn-outline-danger")
-    div.appendChild(clearbtn)
-    clearbtn.addEventListener("click",()=>{
-        div.innerHTML=""
-    })
-
+    clearBTN()
     let ol=document.createElement("ol")
     div.appendChild(ol)
     ol.setAttribute("class","m-2")
     axios.get("https://jsonplaceholder.typicode.com/users")
     .then(resp =>{
-        for(i=1;i<=resp.data.length;i++){
-            axios.get(`https://jsonplaceholder.typicode.com/users/${i}`)
-                .then(response => {
-                    let dv=document.createElement("dv")
-                    let li=document.createElement("li")
-                    let p2=document.createElement("p")
-                    dv.setAttribute("class","ms-4 d-flex  justify-content-between")
-                    li.setAttribute("class","pt-4 pb-4")
-                    p2.setAttribute("class","rounded border border-primary m-4 p-3")
-                    ol.appendChild(dv)
-                    dv.appendChild(li)
-                    dv.appendChild(p2)
-
-                    li.innerHTML="id: "+response.data.id+"<br>"+"name:  "+response.data.name+"<br>"+"username:  "+response.data.username+"<br>"+"e-mail:  "+response.data.email+"<br>"+"address: "+"<ul type='none' class='ms-3 ps-5'"+"<li>"+"street: "+response.data.address.street+"</li>"+"<li>"+"suite: "+response.data.address.suite+"</li>"+"<li>"+"city: "+response.data.address.city+"</li>"+"<li>"+"zipcode: "+response.data.address.zipcode+"</li>"+"<li>"+"geo: "+"<ul class='ms-3 ps-5'>"+"<li>"+"lat: "+response.data.address.geo.lat+"</li>"+"<li>"+"lng: "+response.data.address.geo.lng+"</li>"
-                    p2.innerHTML="phone: "+response.data.phone+"<br>"+"website: "+"<a href='response.data.website'>"+response.data.website+"</a>"+"<br>"+"company: "+"<ul>"+"<li>"+"name: "+response.data.company.name+"</li>"+"<li>"+"catchPhrase: "+response.data.company.catchPhrase+"</li>"+"<li>"+"bs: "+response.data.company.bs+"</li>"
-                    
-                    let hr=document.createElement("hr")
-                    hr.setAttribute("class","m-1")
-                    ol.appendChild(hr)
-                    }   
-                    )}    
+        for(i=1;i<=resp.data.length-1;i++){
+            response=resp.data[i]
+            let dv=document.createElement("div")
+            let li=document.createElement("li")
+            let p2=document.createElement("p")
+            dv.setAttribute("class","ms-4 d-flex  justify-content-between")
+            li.setAttribute("class","pt-4 pb-4")
+            p2.setAttribute("class","rounded border border-primary m-4 p-3")
+            ol.appendChild(dv)
+            dv.appendChild(li)
+            dv.appendChild(p2)
+            li.innerHTML="id: "+response.id+"<br>"+"name:  "+response.name+"<br>"+"username:  "+response.username+"<br>"+"e-mail:  "+response.email+"<br>"+"address: "+"<ul type='none' class='ms-3 ps-5'"+"<li>"+"street: "+response.address.street+"</li>"+"<li>"+"suite: "+response.address.suite+"</li>"+"<li>"+"city: "+response.address.city+"</li>"+"<li>"+"zipcode: "+response.address.zipcode+"</li>"+"<li>"+"geo: "+"<ul class='ms-3 ps-5'>"+"<li>"+"lat: "+response.address.geo.lat+"</li>"+"<li>"+"lng: "+response.address.geo.lng+"</li>"
+            p2.innerHTML="phone: "+response.phone+"<br>"+"website: "+"<a href='response.website'>"+response.website+"</a>"+"<br>"+"company: "+"<ul>"+"<li>"+"name: "+response.company.name+"</li>"+"<li>"+"catchPhrase: "+response.company.catchPhrase+"</li>"+"<li>"+"bs: "+response.company.bs+"</li>"
+            
+            let hr=document.createElement("hr")
+            hr.setAttribute("class","m-1")
+            ol.appendChild(hr)
+        }   
     })
 }
       
