734cfa643e90d3af3566c0b4d143c89c778f0080
garaevo3 <5mptrbf@code.edu.az>
2022-11-14 17:52:41 +0400

    updated

--- task-1/app.js
+++ task-1/app.js
@@ -5,35 +5,48 @@ const input=document.getElementById("input")
 const box=document.getElementById("box")  
 const buttons=document.getElementById("buttons")   
 
-
-let count=0
+let count=0,clicked=2
 
 BTN.addEventListener("click", btn)
-BTN.addEventListener("click", clear)
-mode.addEventListener("click",changeMode)
+
+function newbye(){
+mode.innerHTML="<i style='color: white;'  class='icon fa-solid fa-moon'></i> "
+BTN.setAttribute("class","rounded-0 btn btn-success btn-sm me-3")
+}
+
+mode.addEventListener('click',changeMode)
 
 function changeMode(){
-    mode.innerHTML="<i style='color: white;border-color: black;border-radius: 0.25em;'  class='fa-solid fa-moon'></i> "
-    var element=document.body
-    element.tagName.toggle(body)
+    body.classList.toggle('darkmode')
+    if(body.className=="darkmode"){
+        newbye()
+    }
+    else{
+        BTN.setAttribute("class","rounded-0 btn btn-outline-success btn-sm me-3")
+        mode.innerHTML="<i class='icon fa-solid fa-moon'></i> "
+    }
 }
-// mode.innerHTML="<i style='border-color: black;border-radius: 0.25em'class='fa-solid fa-moon ></i>"
-function clear(){
-    if(count==0){
-    const  button=document.createElement("button")
-    button.setAttribute("class","rounded-0 btn btn-outline-danger btn-sm")
-    button.style.width="82px"
-    button.innerHTML="Clear!"
-    buttons.appendChild(button) 
-    count++
-    button.addEventListener("click",() =>{
-        box.innerHTML=""
-    })} 
-}
+
 function btn() {
+    if(input.value==""){
+        alert("İNPUT is empty!")
+    }
+    else {
+        if(count==0){
+        const  button=document.createElement("button")
+        button.setAttribute("class","rounded-0 btn btn-outline-danger btn-sm")
+        button.style.width="82px"
+        button.innerHTML="Clear!"
+        buttons.appendChild(button) 
+        count++
+        button.addEventListener("click",() =>{
+            box.innerHTML=""
+        })
+    }
+    }
     let p=document.createElement("p")
     p.innerHTML+=input.value
-    p.setAttribute("class","ms-5 ps-4")
+    p.setAttribute("class","ptag")
     box.appendChild(p)
     p.addEventListener("click",pclicked)
     input.value=""
